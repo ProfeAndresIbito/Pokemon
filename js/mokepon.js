54112192fb45bcd11ffc6ab22dec20b8b669589b
ProfeAndresIbito <andresibito@comfandi.edu.co>
2025-03-13 12:56:15 -0500

    Update mokepon.js

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,5 +1,7 @@
 let ataqueJugador
 let ataqueEnemigo
+let vidasJugador=3
+let vidasEnemigo=3
 
 function iniciarJuego(){
     let botonMascotaJugador=document.getElementById("boton-mascota")
@@ -77,6 +79,9 @@ function ataqueAleatorioEnemigo(){
 }
 
 function combate(){
+    let spanVidasJugador=document.getElementById("vidas-jugador")
+    let spanVidasEnemigo=document.getElementById("vidas-enemigo")
+
     if(ataqueEnemigo==ataqueJugador){
         crearMensaje("EMPATE")
     }else if(ataqueJugador=="FUEGO" && ataqueEnemigo=="TIERRA"){
@@ -87,6 +92,8 @@ function combate(){
         crearMensaje("GANASTE")
     }else{
         crearMensaje("PERDISTE")
+        vidasJugador--
+        spanVidasJugador.innerHTML=vidasJugador
     }
 }
 
